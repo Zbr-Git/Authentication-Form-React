8ea9c147e6db934d6519bdafa98e9fb5d9cbcb99
Zubair <zubair.cache@gmail.com>
2024-06-27 23:35:50 +0500

    signup and login page with Formik

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,38 +1,127 @@
-
+import { Formik } from "formik";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    const formData = new FormData();
-  return (
  
-    <div class="min-h-screen flex items-center justify-center w-full white:bg-gray-950">
-        <div class=" bg-white-100	 shadow-md rounded-lg px-8 py-6 max-w-md">
-            <h1 class="text-2xl font-bold text-center mb-4 dark">Welcome Back!</h1>
-            <form action="#">
-                <div class="mb-4">
-                    <label for="email" class="block text-sm font-medium text-gray-700 dark mb-2">Email Address</label>
-                    <input type="email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="your@email.com" required />
-                </div>
-                <div class="mb-4">
-                    <label for="password" class="block text-sm font-medium text-gray-700 dark mb-2">Password</label>
-                    <input type="password" id="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" placeholder="Enter your password" required />
-                    <a href="#"
-                        class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Forgot
-                        Password?</a>
-                </div>
-                <div class="flex items-center justify-between mb-4">
-                    <div class="flex items-center">
-                        <input type="checkbox" id="remember" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none" checked />
-                        <label for="remember" class="ml-2 block text-sm text-gray-700 dark">Remember me</label>
-                    </div>
-                    <a href="#"
-                        class="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create
-                        Account</a>
-                </div>
-                <button onclick='' type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Login</button>
-            </form>
-        </div>
-    </div>
-  )
-}
+  return (
+    <div className="white:bg-gray-950 flex min-h-screen w-full items-center justify-center">
+      <div className="bg-white-100 max-w-md rounded-lg px-8 py-6 shadow-md">
+        <h1 className="dark mb-4 text-center text-2xl font-bold">
+          Welcome Back!
+        </h1>
 
-export default Login
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="dark mb-2 block text-sm font-medium text-gray-700"
+                >
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  className="w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                  placeholder="your@email.com"
+                  
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="dark mb-2 block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                {errors.email && touched.email && errors.email}
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  className="w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                  placeholder="Enter your password"
+                  
+                  autoComplete="on"
+                />
+                <Link
+                  to="/forget-password"
+                  className="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Forgot Password?
+                </Link>
+              </div>
+              <div className="mb-4 flex items-center justify-between">
+                <div className="flex items-center">
+                  <input
+                    type="checkbox"
+                    id="remember"
+                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:outline-none focus:ring-indigo-500"
+                    defaultChecked
+                  />
+                  <label
+                    htmlFor="remember"
+                    className="dark ml-2 block text-sm text-gray-700"
+                  >
+                    Remember me
+                  </label>
+                </div>
+                <Link
+                  to="/signup"
+                  className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  Create Account
+                </Link>
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Login
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
